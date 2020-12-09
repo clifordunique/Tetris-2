d2eef686463e6bfcf0cc6641b8dca494d9c4e26f
유인 한 <gksyy123@naver.com>
2020-12-09 17:25:51 +0900

    2D_tetris_ move and rotate(for GroupI Blocks)

---
+++ Tetris_Game/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public List<GameObject> blocks;
+
+    public Transform spawnPoint;
+
+    private GameObject dir;
+
+    private float timer;
+    
+
+    float fall = 0;
+
+    public float fallingSpeed;
+    private void Start()
+    {
+        dir = Instantiate(blocks[0], spawnPoint);
+    }
+    void Update()
+    {
+        CheckUserInput();
+    }
+    void FixedUpdate()
+    {
+        ApplyGravity();
+    }
+    void CheckUserInput()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            dir.transform.position += new Vector3(1, 0, 0);
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            dir.transform.position += new Vector3(-1, 0, 0);
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            dir.transform.Rotate(0, 0, 90);
+        }
+        else if ((Input.GetKeyDown(KeyCode.DownArrow) || Time.time - fall >= fallingSpeed) && dir.transform.position.y > 3)
+        {
+            Debug.Log(dir.transform.position.y);
+            dir.transform.position += new Vector3(0, -1, 0);
+            fall = Time.time;
+        }
+    }
+
+    public void ApplyGravity()
+    {
+        if (!dir.GetComponent<tetris>().gravityApply)
+        {
+            if ((timer += Time.deltaTime) > 0.2f)
+            {
+                dir.transform.position += new Vector3(0, -1, 0);
+                timer = 0.0f;
+            }
+        }
+        else
+        {
+            dir=Instantiate(blocks[0], spawnPoint);
+        }
+    }
+}
---
+++ Tetris_Game/Assets/Scripts/tetris.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tetris : MonoBehaviour
+{
+    public bool gravityApply;
+    private void Start()
+    {
+        gravityApply = false;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log(collision.gameObject.tag);
+        if (collision.gameObject.tag == "BottomCollider")
+        {
+            gravityApply = true;
+        } 
+    }
+}
