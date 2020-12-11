205257aa04b2109e46d47d8f8b7cf60bcea76412
유인 한 <gksyy123@naver.com>
2020-12-11 17:35:28 +0900

    new tetris falling and set rotation pos

--- Tetris_Game/Assets/Scripts/SpawnManager.cs
+++ Tetris_Game/Assets/Scripts/SpawnManager.cs
@@ -12,11 +12,17 @@ public class SpawnManager : MonoBehaviour
 
     private float timer;
     public bool swithTheBlock;
+    private float previousTime;
+    public float fallTime = 0.8f;
 
+    public static int height = 20;
+    public static int width = 10;
 
     float fall = 0;
 
     public float fallingSpeed;
+
+    
     private void Start()
     {
         dir = Instantiate(blocks[0], spawnPoint);
@@ -77,10 +83,46 @@ public class SpawnManager : MonoBehaviour
         RaycastHit hit;
         if(Physics.Raycast(ray, out hit))
         {
+            Debug.DrawLine(dir.transform.position, hit.point, Color.green);
             if(hit.transform.tag == "Block")
             {
                 dir.GetComponent<tetris>().gravityApply = true;
             }
         }
+    }
+
+    //그냥 천천히 1칸씩 떨어지는 테트리스 
+    //void Fall()
+    //{
+    //    if(Time.time - previousTime > fallTime)
+    //    {
+    //        transform.position += new Vector3(0, -1, 0);
+    //        previousTime = Time.time;
+    //    }
+    //}
+
+    //void Fall2()
+    //{
+    //    if(Time.time - previousTime > (Input.GetKey(KeyCode.DownArrow) ? fallTime / 10 : fallTime))
+    //    {
+    //        transform.position += new Vector3(0, -1, 0);
+    //        previousTime = Time.time;
+    //    }
+    //}
+
+    bool ValidMove()
+    {
+        foreach(Transform children in transform)
+        {
+            int roundedX = Mathf.RoundToInt(children.transform.position.x);
+            int roundedY = Mathf.RoundToInt(children.transform.position.y);
+
+            if(roundedX < 0 || roundedX >= width || roundedY < 0 || roundedY >= height)
+            {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
---
+++ Tetris_Game/Assets/Scripts/SpawnTetris.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnTetris : MonoBehaviour
+{
+    public GameObject[] Tetris;
+    // Start is called before the first frame update
+    void Start()
+    {
+        NewTetris();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void NewTetris()
+    {
+        Instantiate(Tetris[Random.Range(0, Tetris.Length)], transform.position, Quaternion.identity);
+    }
+}
